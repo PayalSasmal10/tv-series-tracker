fe24f9f26f87864c8579b520d810d809624cd479
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-29 15:29:04 +0530

    header is completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import classes from "./Header.module.css";
 
 const Header = () => {
     return (
-        <input id="search" type="text" placeholder="Search TV Series..." />
+        <div className={classes.header}>
+            <a className={classes.home} href="#home">Home</a>
+            <a href="#about">About</a>
+            <input id="search" type="text" placeholder="Search TV Series..." />
+        </div>
+        
     );
 };
 
