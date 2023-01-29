6a2ff7dba42fd510d3806384900cfb8879efb70a
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-29 19:15:51 +0530

    searchbar css is fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import classes from "./Header.module.css";
+import {BsSearch} from 'react-icons/bs';
 
 const Header = () => {
     return (
         <div className={classes.header}>
             <a className={classes.home} href="#home">Home</a>
             <a href="#about">About</a>
-            <input id="search" type="text" placeholder="Search TV Series..." />
+            <input className={classes.search} type="text" />
+            <button className={classes.btn} type="submit"><BsSearch/></button>
         </div>
         
     );
