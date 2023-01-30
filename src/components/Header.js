2b4e3c8473262768f517a85052d58311781f4002
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-30 22:09:02 +0530

    header searchbar is working as a button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import classes from "./Header.module.css";
 import {BsSearch} from 'react-icons/bs';
+import { useState } from "react";
 
 const Header = () => {
+    const [active, setActive] = useState(false);
+
+    const searchButtonHandler = () => {
+        setActive(!active);
+    };
+
     return (
         <div className={classes.header}>
             <a className={classes.home} href="#home">Home</a>
             <a href="#about">About</a>
-            <input className={classes.search} type="text" />
-            <button className={classes.btn} type="submit"><BsSearch/></button>
+            <input className={active?classes.search:classes.input} type="text" placeholder="Search the TV series"/>
+            <button className={classes.btn} type="submit" onClick={searchButtonHandler}><BsSearch/></button>
         </div>
         
     );
