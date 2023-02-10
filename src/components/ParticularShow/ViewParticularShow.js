cd33452513f6fce458903dd22f56863f5029330d
Payal Sasmal <sasmalpayal@gmail.com>
2023-02-10 23:19:11 +0530

    fixed the css

--- src/components/ParticularShow/ViewParticularShow.js
+++ src/components/ParticularShow/ViewParticularShow.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import classes from "./ViewParticularShow.module.css";
 
+
 const ViewParticularShow = ({ selectedSeries, setSelectedSeries }) => {
   const {
     network,
     image,
     status,
     runtime,
+    type,
     genres,
     summary,
     schedule,
@@ -39,26 +41,28 @@ const ViewParticularShow = ({ selectedSeries, setSelectedSeries }) => {
   useEffect(() => {
     if (seriesId) {
       fetchParticularShow(seriesId);
+      setIsViewLoading(false);
     }
   }, [seriesId]);
 
-  if (isViewLoading) {
-    return <p>Loading</p>;
+  if (!isViewLoading) {
+    return <div className={classes.ring}>Loading</div>
   }
 
   return (
-    <div>
-      <div className={classes.imgdiv}>
+    <div className={classes.details}>
         <img className={classes.img} src={image?.original} />
+      <div className={classes.focusdetails}>
+        <p dangerouslySetInnerHTML={{ __html: summary }} />
       </div>
-      <div className={classes.details}>
-        <br></br>
+      <div className={classes.details_sub}>
+        {/* <br></br> */}
         <h1 className={classes.focusdetails}>Show Info</h1>
         <div className={classes.focusdetails}><span className={classes.genre}>Network: </span><a href={network?.officialSite}>{network?.name}</a></div>
-        <div className={classes.focusdetails}><span className={classes.genre}>Schedule: </span>{schedule?.days} at {schedule?.time}( {runtime}m)</div>
+        <div className={classes.focusdetails}><span className={classes.genre}>Schedule: </span>{schedule?.days} at {schedule?.time} ({runtime}m)</div>
         <div className={classes.focusdetails}><span className={classes.genre}>Country: </span>{network?.country.name}</div>
         <div className={classes.focusdetails}><span className={classes.genre}>Status: </span> {status}</div>
-        <div className={classes.focusdetails}><span className={classes.genre}>Total Runtime: </span> {runtime}m</div>
+        <div className={classes.focusdetails}><span className={classes.genre}>Show Type: </span> {type}</div>
         <div className={classes.focusdetails}>
         <span className={classes.genre}>Genres: </span>{" "}
           {genres?.map((genre, idx) => (
@@ -68,12 +72,10 @@ const ViewParticularShow = ({ selectedSeries, setSelectedSeries }) => {
             </span>
           ))}
         </div>
-        <div className={classes.focusdetails}><span className={classes.genre}>Official site : </span> <a href={officialSite}>{network?.officialSite}</a></div>
-        <br></br>
+        <div className={classes.focusdetails}><span className={classes.genre}>Official site : </span><a href={officialSite}>{officialSite}</a></div>
+        
       </div>
-      <div className={classes.contents}>
-        <p dangerouslySetInnerHTML={{ __html: summary }} />
-      </div>
+      
     </div>
   );
 };
