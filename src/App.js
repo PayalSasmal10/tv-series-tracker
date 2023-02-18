a32ba414d59b508717ac06e70a5720238a19cfc1
Payal Sasmal <sasmalpayal@gmail.com>
2023-02-18 21:54:15 +0530

    set the favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-import AvailableSeries from "./components/Series/AvailableSeries";
 import { useEffect, useState } from "react";
 import { RouterProvider, createHashRouter } from "react-router-dom";
+import AvailableSeries from "./components/Series/AvailableSeries";
 import RootLayout from "./components/Router/Root";
 import ViewParticularShow from "./components/ParticularShow/ViewParticularShow";
+import icon from "./assets/tvicon.png";
+import "./App.css";
 
 const itemsPerPage = 12;
 
@@ -16,6 +17,11 @@ function App() {
   const [selectedSeries, setSelectedSeries] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const favicon = document.getElementById("icon");
+    favicon.setAttribute('href', icon);
+  }, []);
 
   // Loading filtered value
   useEffect(() => {
